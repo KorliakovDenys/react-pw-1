746a670370c25e38cf74985c33497b161e5e5e5f
Denys Korliakov <korl.den66@gmail.com>
2023-10-30 10:57:32 +0200

    Change hobby images and card styles to improve visual appeal and consistency.

--- src/hobbies.js
+++ src/hobbies.js
@@ -3,7 +3,7 @@ const HOBBY_DATA = [
         "id": 1,
         "name": "Photography",
         "description": "Capturing moments through the lens of a camera. ",
-        "img": "https://dummyimage.com/300x250/000/fff",
+        "img": "https://dummyimage.com/300x250/f00/fff",
         "famousPersons": [
             {
                 "name": "Ansel Adams",
@@ -15,19 +15,19 @@ const HOBBY_DATA = [
         "id": 2,
         "name": "Cooking",
         "description": "Creating delicious dishes in the kitchen. Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
-        "img": "https://dummyimage.com/300x250/000/fff",
+        "img": "https://dummyimage.com/300x250/0f0/fff",
         "famousPersons": [
             {
                 "name": "Julia Child",
-                "img": "https://dummyimage.com/250x300/000/fff"
+                "img": "https://dummyimage.com/250x300/0f0/fff"
             },
             {
                 "name": "Julia Child",
-                "img": "https://dummyimage.com/250x300/000/fff"
+                "img": "https://dummyimage.com/250x300/f00/fff"
             },
             {
                 "name": "Julia Child",
-                "img": "https://dummyimage.com/250x300/000/fff"
+                "img": "https://dummyimage.com/250x300/00f/fff"
             },
             {
                 "name": "Julia Child",
@@ -47,7 +47,7 @@ const HOBBY_DATA = [
         "id": 3,
         "name": "Gardening",
         "description": "Cultivating plants and creating beautiful gardens.",
-        "img": "https://dummyimage.com/300x250/000/fff",
+        "img": "https://dummyimage.com/300x250/00f/fff",
         "famousPersons": [
             {
                 "name": "Monty Don",
